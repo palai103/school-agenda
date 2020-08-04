da807a8ff0349852521f61b349a695f44cdab4fa
Ubaldo Puocci <u.puocci@gmail.com>
2020-08-04 23:25:29 +0200

    Finished unit tests for the CLI

--- src/main/java/view/AgendaViewCli.java
+++ src/main/java/view/AgendaViewCli.java
@@ -1,150 +1,258 @@
 package view;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.List;
+import java.util.Scanner;
 
 import controller.AgendaController;
 import model.Course;
 import model.Student;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Model.CommandSpec;
-import picocli.CommandLine.Spec;
 
 @Command
-public class AgendaViewCli implements AgendaView, Runnable {
+public class AgendaViewCli implements AgendaView {
 
-	AgendaController controller;
+	private static final String STUDENT_WITH_ID = "Student with id ";
+	private static final String INSERT_COURSE_ID = "Insert course id: ";
+	private static final String COURSE_WITH_ID = "Course with id ";
+	private static final String INSERT_STUDENT_ID = "Insert student id: ";
+	private AgendaController controller;
+	private InputStream inputStream;
+	private PrintStream printStream;
+	private Scanner scanner;
 
-	@Spec
-	CommandSpec spec;
-
-	private <T> void printToOutput(T toPrint) {
-		spec.commandLine().getOut().println(toPrint);
-	}
-
-	@Command(name = "getStudents")
-	public void showAllStudentsCLI() {
-		controller.getAllStudents();
+	public AgendaViewCli(InputStream inputStream, PrintStream printStream) {
+		this.inputStream = inputStream;
+		this.printStream = printStream;
 	}
 
 	@Override
 	public void showAllStudents(List<Student> allStudents) {
-		System.out.println(spec);
-		printToOutput("Students:");
 		for (Student student : allStudents) {
-			printToOutput(student.toString());
+			printStream.println(student.toString());
 		}
 	}
 
 	@Override
 	public void notifyStudentAdded(Student student) {
-		printToOutput("Added student:");
-		printToOutput(student.toString());
+		printStream.println("Added " + student.toString());
 	}
 
 	@Override
 	public void notifyStudentNotAdded(Student student) {
-		printToOutput("Not added student:");
-		printToOutput(student.toString());
+		printStream.println(student.toString() + " not added");
 	}
 
 	@Override
 	public void notifyStudentRemoved(Student student) {
-		// TODO Auto-generated method stub
-
+		printStream.println("Removed " + student.toString());
 	}
 
 	@Override
 	public void notifyStudentNotRemoved(Student student) {
-		// TODO Auto-generated method stub
-
+		printStream.println(STUDENT_WITH_ID + student.getId() + " not removed");
 	}
 
 	@Override
 	public void notifyCourseAddedToStudent(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(COURSE_WITH_ID + course.getId() + " added to student with id " + student.getId());
 	}
 
 	@Override
 	public void notifyCourseNotAddedToStudent(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(COURSE_WITH_ID + course.getId() + " not added to student with id " + student.getId());
 	}
 
 	@Override
 	public void notifyCourseRemovedFromStudent(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(COURSE_WITH_ID + course.getId() + " removed from student with id " + student.getId());
 	}
 
 	@Override
 	public void notifyCourseNotRemovedFromStudent(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(COURSE_WITH_ID + course.getId() + " not removed from student with id " + student.getId());
 	}
 
 	@Override
 	public void notifyCourseAdded(Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println("Added " + course.toString());
 	}
 
 	@Override
 	public void notifyCourseNotAdded(Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(course.toString() + " not added");
 	}
 
 	@Override
 	public void notifyCourseRemoved(Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println("Removed " + course.toString());
 	}
 
 	@Override
 	public void notifyCourseNotRemoved(Course course) {
-		// TODO Auto-generated method stub
+		printStream.println(course.toString() + " not removed");
 
 	}
 
 	@Override
 	public void notifyStudentRemovedFromCourse(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(STUDENT_WITH_ID + student.getId() + " removed from course with id " + course.getId());
 	}
 
 	@Override
 	public void notifyStudentNotRemovedFromCourse(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(STUDENT_WITH_ID + student.getId() + " not removed from course with id " + course.getId());
 	}
 
 	@Override
 	public void notifyStudentNotAddedToCourse(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(STUDENT_WITH_ID + student.getId() + " not added to course with id " + course.getId());
 	}
 
 	@Override
 	public void notifyStudentAddedToCourse(Student student, Course course) {
-		// TODO Auto-generated method stub
-
+		printStream.println(STUDENT_WITH_ID + student.getId() + " added to course with id " + course.getId());
 	}
 
 	@Override
 	public void showAllCourses(List<Course> allCourses) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-
+		for (Course course : allCourses) {
+			printStream.println(course.toString());
+		}
 	}
 
 	public void inject(AgendaController controller) {
 		this.controller = controller;
+
+	}
+
+	public void showMenu() {
+		printStream.println("--------- Pick a choice: ---------\r\n" + "1) Show all students\r\n"
+				+ "2) Show all courses\r\n" + "3) Add a student\r\n" + "4) Add a course\r\n"
+				+ "5) Enroll a student to a course (by student)\r\n" + "6) Enroll a student to a course (by course)\r\n"
+				+ "7) Delete a student enrollment (by student id)\r\n"
+				+ "8) Delete a student enrollment (by course id)\r\n" + "9) Delete a student\r\n"
+				+ "10) Delete a course\r\n" + "11) Exit\r\n" + "---------------------------------");
+	}
+
+	public void setInput(ByteArrayInputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	public void menuChoice() {
+		showMenu();
+
+		scanner = new Scanner(inputStream);
+		String choice = scanner.nextLine();
+
+		switch (choice) {
+		case "1":
+			showAllStudentsCallController();
+			break;
+		case "2":
+			showAllCoursesCallController();
+			break;
+		case "3":
+			addStudentCallController();
+			break;
+		case "4":
+			addCourseCallController();
+			break;
+		case "5":
+			addCourseToStudentCallController();
+			break;
+		case "6":
+			addStudentToCourseCallController();
+			break;
+		case "7":
+			removeCourseToStudentCallController();
+			break;
+		case "8":
+			removeStudentToCourseCallController();
+			break;
+		case "9":
+			removeStudentCallController();
+			break;
+		case "10":
+			removeCourseCallControler();
+			break;
+		case "11":
+			System.exit(0);
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	private void removeStudentToCourseCallController() {
+		printStream.print(INSERT_STUDENT_ID);
+		String studentId = scanner.nextLine();
+		printStream.print(INSERT_COURSE_ID);
+		String courseId = scanner.nextLine();
+		controller.removeStudentFromCourse(new Student(studentId, ""), new Course(courseId, ""));
+	}
+
+	private void addStudentToCourseCallController() {
+		printStream.print(INSERT_STUDENT_ID);
+		String studentId = scanner.nextLine();
+		printStream.print(INSERT_COURSE_ID);
+		String courseId = scanner.nextLine();
+		controller.addStudentToCourse(new Student(studentId, ""), new Course(courseId, ""));
+	}
+
+	private void removeCourseCallControler() {
+		printStream.print(INSERT_COURSE_ID);
+		String id = scanner.nextLine();
+		controller.removeCourse(new Course(id, ""));
+	}
+
+	private void addCourseCallController() {
+		printStream.print(INSERT_COURSE_ID);
+		String id = scanner.nextLine();
+		printStream.print("Insert course name: ");
+		String name = scanner.nextLine();
+		controller.addCourse(new Course(id, name));
+	}
+
+	private void removeCourseToStudentCallController() {
+		printStream.print(INSERT_STUDENT_ID);
+		String studentId = scanner.nextLine();
+		printStream.print(INSERT_COURSE_ID);
+		String courseId = scanner.nextLine();
+		controller.removeCourseFromStudent(new Student(studentId, ""), new Course(courseId, ""));
+	}
+
+	private void addCourseToStudentCallController() {
+		printStream.print(INSERT_STUDENT_ID);
+		String studentId = scanner.nextLine();
+		printStream.print(INSERT_COURSE_ID);
+		String courseId = scanner.nextLine();
+		controller.addCourseToStudent(new Student(studentId, ""), new Course(courseId, ""));
+	}
+
+	private void removeStudentCallController() {
+		printStream.print(INSERT_STUDENT_ID);
+		String id = scanner.nextLine();
+		controller.removeStudent(new Student(id, ""));
+	}
+
+	private void addStudentCallController() {
+		printStream.print(INSERT_STUDENT_ID);
+		String id = scanner.nextLine();
+		printStream.print("Insert student name: ");
+		String name = scanner.nextLine();
+		controller.addStudent(new Student(id, name));
+	}
+
+	private void showAllStudentsCallController() {
+		controller.getAllStudents();
+	}
+
+	private void showAllCoursesCallController() {
+		controller.getAllCourses();
 	}
 
 }
--- src/test/java/view/AgendaViewTest.java
+++ src/test/java/view/AgendaViewTest.java
@@ -2,11 +2,11 @@ package view;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,47 +15,422 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import controller.AgendaController;
+import model.Course;
 import model.Student;
-import picocli.CommandLine;
-import service.AgendaService;
 
 public class AgendaViewTest {
 
-	private CommandLine cmd;
-
 	@Mock
-	private AgendaService service;
-
-	@InjectMocks
 	private AgendaController controller;
 
 	@InjectMocks
 	private AgendaViewCli cliView;
 
-	private StringWriter sw;
+	private ByteArrayOutputStream testOutput;
+	private ByteArrayInputStream testInput;
 
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
-		cliView = new AgendaViewCli();
+		testOutput = new ByteArrayOutputStream();
+		cliView = new AgendaViewCli(System.in, new PrintStream(testOutput));
 		cliView.inject(controller);
-		controller = new AgendaController(cliView, service);
-		cmd = new CommandLine(cliView);
-		sw = new StringWriter();
-		cmd.setOut(new PrintWriter(sw));
+	}
+
+	@Test
+	public void testMenu() {
+		// exercise
+		cliView.showMenu();
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("--------- Pick a choice: ---------\r\n"
+				+ "1) Show all students\r\n" + "2) Show all courses\r\n" + "3) Add a student\r\n"
+				+ "4) Add a course\r\n" + "5) Enroll a student to a course (by student)\r\n"
+				+ "6) Enroll a student to a course (by course)\r\n"
+				+ "7) Delete a student enrollment (by student id)\r\n"
+				+ "8) Delete a student enrollment (by course id)\r\n" + "9) Delete a student\r\n"
+				+ "10) Delete a course\r\n" + "11) Exit\r\n" + "---------------------------------\r\n");
 	}
 
 	@Test
 	public void testShowAllStudents() {
 		// setup
-		Student testStudent = new Student("1", "testStudent");
-		List<Student> allStudents = asList(testStudent);
-		when(service.getAllStudents()).thenReturn(allStudents);
+		testInput = new ByteArrayInputStream("1".getBytes());
+		cliView.setInput(testInput);
 
 		// exercise
-		cmd.execute("getStudents");
+		cliView.menuChoice();
 
 		// verify
-		assertThat("Students:\n" + "Student [id=1, name=testStudent]\n").isEqualTo(sw.toString());
+		verify(controller).getAllStudents();
+	}
+
+	@Test
+	public void testPrintShowAllStudents() {
+		// setup
+		Student testStudent1 = new Student("1", "test student 1");
+		Student testStudent2 = new Student("2", "test student 2");
+
+		// exercise
+		cliView.showAllStudents(asList(testStudent1, testStudent2));
+
+		// verify
+		assertThat(testOutput.toString())
+				.hasToString("Student [id=1, name=test student 1]\r\n" + "Student [id=2, name=test student 2]\r\n");
+	}
+
+	@Test
+	public void testNotifyStudentAdded() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+
+		// exercise
+		cliView.notifyStudentAdded(testStudent);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Added " + testStudent.toString() + "\r\n");
+	}
+
+	@Test
+	public void testAddStudent() {
+		// Setup
+		String userInput = "3\n1\ntest student";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// Exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert student id: Insert student name: ");
+		verify(controller).addStudent(new Student("1", "test student"));
+	}
+
+	@Test
+	public void testNotifyStudentNotAdded() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+
+		// exercise
+		cliView.notifyStudentNotAdded(testStudent);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString(testStudent.toString() + " not added\r\n");
+	}
+
+	@Test
+	public void testNotifyStudentRemoved() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+
+		// exercise
+		cliView.notifyStudentRemoved(testStudent);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Removed " + testStudent.toString() + "\r\n");
+	}
+
+	@Test
+	public void testRemoveStudent() {
+		// Setup
+		String userInput = "9\n1";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// Exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert student id: ");
+		verify(controller).removeStudent(new Student("1", ""));
+	}
+
+	@Test
+	public void testNotifyStudentNotRemoved() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+
+		// exercise
+		cliView.notifyStudentNotRemoved(testStudent);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Student with id " + testStudent.getId() + " not removed\r\n");
+	}
+
+	@Test
+	public void testNotifyCourseAddedToStudent() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyCourseAddedToStudent(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString(
+				"Course with id " + testCourse.getId() + " added to student with id " + testStudent.getId() + "\r\n");
+	}
+
+	@Test
+	public void testAddCourseToStudent() {
+		// setup
+		String userInput = "5\n1\n2";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert student id: Insert course id: ");
+		verify(controller).addCourseToStudent(new Student("1", ""), new Course("2", ""));
+	}
+
+	@Test
+	public void testNotifyCourseNotAddedToStudent() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyCourseNotAddedToStudent(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Course with id " + testCourse.getId()
+				+ " not added to student with id " + testStudent.getId() + "\r\n");
+	}
+
+	@Test
+	public void testNotifyCourseRemovedFromStudent() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyCourseRemovedFromStudent(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Course with id " + testCourse.getId()
+				+ " removed from student with id " + testStudent.getId() + "\r\n");
+	}
+
+	@Test
+	public void testRemoveCourseFromStudent() {
+		// setup
+		String userInput = "7\n1\n2";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert student id: Insert course id: ");
+		verify(controller).removeCourseFromStudent(new Student("1", ""), new Course("2", ""));
+	}
+
+	@Test
+	public void testNotifyCourseNotRemovedFromStudent() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyCourseNotRemovedFromStudent(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Course with id " + testCourse.getId()
+				+ " not removed from student with id " + testStudent.getId() + "\r\n");
+	}
+
+	@Test
+	public void testNotifyCourseAdded() {
+		// setup
+		Course testCourse = new Course("1", "test course");
+
+		// exercise
+		cliView.notifyCourseAdded(testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Added " + testCourse.toString() + "\r\n");
+	}
+
+	@Test
+	public void testAddCourse() {
+		// Setup
+		String userInput = "4\n1\ntest course";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// Exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert course id: Insert course name: ");
+		verify(controller).addCourse(new Course("1", "test course"));
+	}
+
+	@Test
+	public void testNotifyCourseNotAdded() {
+		// setup
+		Course testCourse = new Course("1", "test course");
+
+		// exercise
+		cliView.notifyCourseNotAdded(testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString(testCourse.toString() + " not added\r\n");
+	}
+
+	@Test
+	public void testNotifyCourseRemoved() {
+		// setup
+		Course testCourse = new Course("1", "test course");
+
+		// exercise
+		cliView.notifyCourseRemoved(testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Removed " + testCourse.toString() + "\r\n");
+	}
+
+	@Test
+	public void testRemoveCourse() {
+		// Setup
+		String userInput = "10\n1";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// Exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert course id: ");
+		verify(controller).removeCourse(new Course("1", ""));
+	}
+
+	@Test
+	public void testNotifyCourseNotRemoved() {
+		// setup
+		Course testCourse = new Course("1", "test course");
+
+		// exercise
+		cliView.notifyCourseNotRemoved(testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString(testCourse.toString() + " not removed\r\n");
+	}
+
+	@Test
+	public void testNotifyStudentRemovedFromCourse() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyStudentRemovedFromCourse(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Student with id " + testStudent.getId()
+				+ " removed from course with id " + testCourse.getId() + "\r\n");
+	}
+
+	@Test
+	public void testNotifyStudentNotRemovedFromCourse() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyStudentNotRemovedFromCourse(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Student with id " + testStudent.getId()
+				+ " not removed from course with id " + testCourse.getId() + "\r\n");
+	}
+
+	@Test
+	public void testNotifyStudentNotAddedToCourse() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyStudentNotAddedToCourse(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString("Student with id " + testStudent.getId()
+				+ " not added to course with id " + testCourse.getId() + "\r\n");
+	}
+
+	@Test
+	public void testAddStudentToCourse() {
+		// setup
+		String userInput = "6\n1\n2";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert student id: Insert course id: ");
+		verify(controller).addStudentToCourse(new Student("1", ""), new Course("2", ""));
+	}
+
+	@Test
+	public void testNotifyStudentAddedToCourse() {
+		// setup
+		Student testStudent = new Student("1", "test student 1");
+		Course testCourse = new Course("2", "test course 2");
+
+		// exercise
+		cliView.notifyStudentAddedToCourse(testStudent, testCourse);
+
+		// verify
+		assertThat(testOutput.toString()).hasToString(
+				"Student with id " + testStudent.getId() + " added to course with id " + testCourse.getId() + "\r\n");
+	}
+
+	@Test
+	public void testShowAllCourses() {
+		// setup
+		testInput = new ByteArrayInputStream("2".getBytes());
+		cliView.setInput(testInput);
+
+		// exercise
+		cliView.menuChoice();
+
+		// verify
+		verify(controller).getAllCourses();
+	}
+
+	@Test
+	public void testPrintShowAllCourses() {
+		// setup
+		Course testCourse1 = new Course("1", "test course 1");
+		Course testCourse2 = new Course("2", "test course 2");
+
+		// exercise
+		cliView.showAllCourses(asList(testCourse1, testCourse2));
+
+		// verify
+		assertThat(testOutput.toString())
+				.hasToString("Course [id=1, name=test course 1]\r\n" + "Course [id=2, name=test course 2]\r\n");
+	}
+
+	@Test
+	public void testRemoveStudentFromCourse() {
+		// setup
+		String userInput = "8\n1\n2";
+		testInput = new ByteArrayInputStream(userInput.getBytes());
+		cliView.setInput(testInput);
+
+		// exercise
+		cliView.menuChoice();
+
+		// verify
+		assertThat(testOutput.toString()).contains("Insert student id: Insert course id: ");
+		verify(controller).removeStudentFromCourse(new Student("1", ""), new Course("2", ""));
 	}
 }
